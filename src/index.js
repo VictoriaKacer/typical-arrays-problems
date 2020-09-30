53e80fe2608e5c51667604e7408282f9c4732516
VictoriaKacer <kacer1703@gmail.com>
2020-09-30 15:25:20 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array) {
+        return 0;
+    }
+    var num = 0,
+        count = 0;
+    for (var count = array.length, i = count - 1; i >= 0; i--) {
+        num = Math.min.apply(Math, array);
+    }
+    return count ? num : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array) {
+        return 0;
+    }
+    var num = 0,
+        count = 0;
+    for (var count = array.length, i = count - 1; i >= 0; i--) {
+        num = Math.max.apply(Math, array);
+    }
+    return count ? num : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array) {
+        return 0;
+    }
+    var sum = 0,
+        count = 0;
+    for (var count = array.length, i = count - 1; i >= 0; i--) {
+        sum += array[i];
+    }
+    return count ? sum / count : 0;
 }
